7f4a3ac9b263170febfa467a1c2b5ed82c3ca3b4
Adithya-b-03 <adithyaakash0304@gmail.com>
2025-06-12 21:51:49 +0530

    e-commerce app updated

--- lib/features/authentication/screens/onboarding/onboarding.dart
+++ lib/features/authentication/screens/onboarding/onboarding.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:t_store/features/authentication/screens/onboarding/widgets/onboarding_dot_navigation.dart';
 import 'package:t_store/features/authentication/screens/onboarding/widgets/onboarding_page.dart';
 import 'package:t_store/features/authentication/screens/onboarding/widgets/onboarding_skip.dart';
+import 'package:t_store/utils/constants/colors.dart';
 import 'package:t_store/utils/helpers/helper_functions.dart';
 import 'package:t_store/utils/constants/sizes.dart';
 import 'package:t_store/utils/constants/image_strings.dart';
 import 'package:t_store/utils/constants/text_strings.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+
 
 import '../../../../utils/device/device_utility.dart';
 class  OnBoardingScreen extends StatelessWidget {
@@ -36,12 +41,34 @@ class  OnBoardingScreen extends StatelessWidget {
               ],
             ),
             const OnBoardingSkip(),
-            // SmoothPageIndicator(controller:PageController(),count:3),
+            const OnBoardingDotNavigation(),
+            const OnBoardingNextButton()
           ],
         )
     );
   }
 }
+
+class OnBoardingNextButton extends StatelessWidget {
+  const OnBoardingNextButton({
+    super.key,
+  });
+  @override
+  Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
+    return Positioned(
+        right: TSizes.defaultSpace,
+        bottom: TDeviceUtils.getBottomNavigationBarHeight(),
+        child: ElevatedButton(
+            onPressed: (){},
+            child: Icon(Iconsax.arrow_right_3,  color: dark ? Colors.black : Colors.white,),
+            style: ElevatedButton.styleFrom(shape: const CircleBorder(),backgroundColor: dark?TColors.light:TColors.black)
+        ),
+    );
+  }
+}
+
+
 
 
 
--- lib/features/authentication/screens/onboarding/widgets/onboarding_page.dart
+++ lib/features/authentication/screens/onboarding/widgets/onboarding_page.dart
@@ -6,7 +6,9 @@ class OnBoardingPage extends StatelessWidget {
   const OnBoardingPage({
     super.key, required this.image, required this.title, required this.subTitle,
   });
-  final String image,title,subTitle;
+
+  final String image, title, subTitle;
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -16,12 +18,19 @@ class OnBoardingPage extends StatelessWidget {
             Image(
               width: THelperFunctions.screenWidth() * 0.8,
               height: THelperFunctions.screenHeight() * 0.6,
-              image:  AssetImage(image),
+              image: AssetImage(image),
             ),
-            Text(title, style: Theme.of(context).textTheme.headlineMedium, textAlign: TextAlign.center),
+            Text(title, style: Theme
+                .of(context)
+                .textTheme
+                .headlineMedium, textAlign: TextAlign.center),
             const SizedBox(height: TSizes.spaceBtwItems),
-            Text(subTitle, style: Theme.of(context).textTheme.bodyMedium, textAlign: TextAlign.center)
+            Text(subTitle, style: Theme
+                .of(context)
+                .textTheme
+                .bodyMedium, textAlign: TextAlign.center)
           ]
       ),
     );
   }
+}
