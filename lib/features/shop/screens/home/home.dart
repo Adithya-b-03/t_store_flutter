41c1ac3c00e2247f7a7776cbef105bcd3fbe2713
Adithya-b-03 <adithyaakash0304@gmail.com>
2025-06-18 21:03:32 +0530

    e-commerce app updated

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:t_store/common/widgets/custom_shapes/curved_edges/curved_edges.dart';
 import 'package:t_store/utils/constants/sizes.dart';
 
 import '../../../../common/widgets/custom_shapes/containers/circular_container.dart';
+import '../../../../common/widgets/custom_shapes/curved_edges/curved_edge_widget.dart';
 import '../../../../utils/constants/colors.dart';
 class THomeScreen extends StatelessWidget {
   const THomeScreen({super.key});
@@ -10,21 +12,27 @@ class THomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        child:Column(
+        child:TCurvedEdgeWidget(child: Column(
           children: [
             Container(
               color:TColors.primary,
               padding: const EdgeInsets.all(0),
-              child: const Stack(
-                children: [
-                  TCircularContainer(child: null,)
-                ]
+              child: const SizedBox(
+                height: 400,
+                child: Stack(
+                    children: [
+                      Positioned(top:-150 ,right:-250,child: TCircularContainer(child: null,)),
+                      Positioned(top:100,right:-300,child: TCircularContainer(child: null,))
+                    ]
+                ),
               ),
             )
           ],
-        )
+        ),)
       )
     );
   }
 }
+
+
 
