d0eeb16dada60e7d2ad128b41540286fe53556da
Adithya-b-03 <adithyaakash0304@gmail.com>
2025-06-21 22:22:31 +0530

    e-commerce app updated

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,30 +1,85 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 import 'package:t_store/common/widgets/appbar/appbar.dart';
 import 'package:t_store/common/widgets/custom_shapes/curved_edges/curved_edges.dart';
 import 'package:t_store/utils/constants/sizes.dart';
+import 'package:t_store/utils/constants/text_strings.dart';
 
 import '../../../../common/widgets/custom_shapes/containers/circular_container.dart';
 import '../../../../common/widgets/custom_shapes/containers/primary_header_container.dart';
 import '../../../../common/widgets/custom_shapes/curved_edges/curved_edge_widget.dart';
 import '../../../../utils/constants/colors.dart';
+
 class THomeScreen extends StatelessWidget {
   const THomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SingleChildScrollView(
-        child:TPrimaryHeaderContainer(child:Column(
-          children: [
-              // TAppBar()
+    return Scaffold(
+        body: SingleChildScrollView(
+            child: TPrimaryHeaderContainer(
+                child: Column(
+      children: [
+        TAppBar(
+          title: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(TTexts.homeAppbarTitle,
+                  style: Theme.of(context)
+                      .textTheme
+                      .labelMedium!
+                      .apply(color: TColors.grey)),
+              Text(TTexts.homeAppbarSubTitle,
+                  style: Theme.of(context)
+                      .textTheme
+                      .headlineSmall!
+                      .apply(color: TColors.white))
+            ],
+          ),
+          actions: [
+            TCartCounter(onPressed: (){},iconColor: TColors.white,)
           ],
         )
-        )
-      )
-    );
+      ],
+    ))));
   }
 }
 
-
-
-
+class TCartCounter extends StatelessWidget {
+  const TCartCounter({
+    super.key, required this.onPressed, required this.iconColor,
+  });
+  final VoidCallback onPressed;
+  final Color iconColor;
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        IconButton(
+          onPressed: onPressed,
+          icon: Icon(
+            Iconsax.shopping_bag,
+            color: iconColor,
+          ),
+        ),
+        Positioned(
+            right: 0,
+            child: Container(
+              width: 18,
+              height: 18,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(100)),
+              child: Center(
+                child: Text(
+                  '2',
+                  style: Theme.of(context)
+                      .textTheme
+                      .labelLarge!
+                      .apply(color: TColors.white, fontSizeFactor: 0.8),
+                ),
+              ),
+            ))
+      ],
+    );
+  }
+}
