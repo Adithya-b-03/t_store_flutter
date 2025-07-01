fd464511900831ed69e99ba2baae7665bda73f92
Adithya-b-03 <adithyaakash0304@gmail.com>
2025-07-01 21:19:53 +0530

    e-commerce app updated

--- lib/common/widgets/products/produts_cards/product_card_vertical.dart
+++ lib/common/widgets/products/produts_cards/product_card_vertical.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 import 'package:t_store/common/styles/shadows.dart';
 import 'package:t_store/common/widgets/custom_shapes/containers/circular_container.dart';
+import 'package:t_store/common/widgets/image/t_rounded_image.dart';
 import 'package:t_store/utils/constants/colors.dart';
 import 'package:t_store/utils/helpers/helper_functions.dart';
-
+import '../../../../utils/constants/image_strings.dart';
 import '../../../../utils/constants/sizes.dart';
+import '../../../../utils/constants/sizes.dart';
+import '../../custom_shapes/containers/rounded_container.dart';
+
 class TProductCardVertical extends StatelessWidget {
   const TProductCardVertical({super.key});
 
@@ -19,11 +24,39 @@ class TProductCardVertical extends StatelessWidget {
         borderRadius: BorderRadius.circular(TSizes.productImageRadius),
         color: dark ? TColors.darkGrey : TColors.white,
       ),
-      child: Column(
-        children:[
-
-        ]
-      ),
+      child: Column(children: [
+        //thumbnail,wishlistbutton,discount tag
+        TRoundedContainer(
+            height: 180,
+            padding: const EdgeInsets.all(TSizes.sm),
+            backgroundColor: dark ? TColors.dark : TColors.light,
+            child: Stack(
+              children: [
+                //thumbail image
+                TRoundedImage(
+                  imageUrl: TImages.productImage1,
+                  applyImageRadius: true,
+                ),
+                //sale tag
+                Positioned(
+                    top: 12,
+                    child: TRoundedContainer(
+                      radius: TSizes.sm,
+                      backgroundColor: TColors.secondary.withOpacity(0.8),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: TSizes.sm, vertical: TSizes.xs),
+                      child: Text('25%',
+                          style: Theme.of(context)
+                              .textTheme
+                              .labelLarge!
+                              .apply(color: TColors.black)),
+                    )),
+                //favourite icon button
+                TCircularIcon(dark: dark)
+              ],
+            ))
+      ]),
     );
   }
 }
+
